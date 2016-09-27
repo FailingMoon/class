e59004e70343a158aca5c79bfb750db30964fefb
pandabran <brandonbenedict.biano@gmail.com>
2016-09-27 14:23:24 +0800

    added remove

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
 <div class="text-center">
 	<?php echo "This is home";
 	echo "<br>";?>
-	<button class="btn btn-danger">Click me</button>
+	<button class="btn btn-danger"><i class="glyphicon glyphicon-remove"></i></button>
 </div>
 </body>
 </html>
